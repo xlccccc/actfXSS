930b30f08a56a6e90abe52c1d31c8cb2fea8afc3
xlccccc <2595251998@qq.com>
2023-10-30 07:14:56 +0800

    4

--- js/base.js
+++ js/base.js
@@ -1,26 +1,32 @@
-let a = window.open('http://124.70.33.170:3000/login','app');
-a = window.open('http://124.70.33.170:3000/login','app');
-const username = 'http://47.115.201.35:7799/';
-const password = '5543f02abbd854192cd335464c038e4c';
-a.fetch('http://124.70.33.170:3000/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: `username=${username}&password=${password}`
-}).then(res => res.text()).then(res => console.log(res));
-a.fetch('http://124.70.33.170:3000/vip', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: `code=123`,
-  redirect: 'follow'
-}).then(response => {
-// 处理VIP页面的响应
-console.log(response);
-})
-.catch(error => {
-// 处理错误
-console.error(error);
-});
+// 访问您的网站
+fetch('http://124.70.33.170:3000/login')
+  .then(response => {
+    // 登录
+    const username = 'http://47.115.201.35:7799/';
+    const password = '5543f02abbd854192cd335464c038e4c';
+    return fetch('http://124.70.33.170:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `username=${username}&password=${password}`
+    });
+  })
+  .then(response => {
+    return fetch('http://124.70.33.170:3000/vip', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `code=123`,
+      redirect: 'follow'
+    });
+  })
+  .then(response => {
+    // 处理VIP页面的响应
+    console.log(response);
+  })
+  .catch(error => {
+    // 处理错误
+    console.error(error);
+  });
